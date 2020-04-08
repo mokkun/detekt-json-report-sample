115d52be55ce1e20afb0ffba3b3d88bbb9f0b6a6
Mozart Petter <mozart@petter.io>
2020-04-08 13:02:15 +0900

    Introduce magic number

--- app/src/main/java/io/petter/playground/FirstFragment.kt
+++ app/src/main/java/io/petter/playground/FirstFragment.kt
@@ -1,11 +1,11 @@
 package io.petter.playground
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -23,6 +23,9 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val magicNumber = 10
+        println(magicNumber)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
